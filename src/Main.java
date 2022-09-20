d7e399d7f8d7def44915f301c48e245c5615c525
iamsayan <sayan.kolay247@gmail.com>
2022-09-20 13:10:36 +0530

    Fibo series

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.println(a);
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        //  int max = a;
+        // if (b>max){
+        //   max = b;
+        // if (c>max){
+        //  max = c;
 
-        System.out.println("Hello world!");
-        // Sayan Koley
+        // System.out.println(max);
+
+        // Find the Largest of the 3 numbers
+
+        int max = Math.max(c, Math.max(a, b));
+        System.out.println(max);
     }
 }
+
+
+
+
+        // Sayan Koley
+
+
+
+
+
+
