3f63de54f3eb5c8558217b9f937d0b6c8467eff2
iamsayan <sayan.kolay247@gmail.com>
2022-09-27 15:40:12 +0530

    Largest Number New code

--- src/LargestNumber.java
+++ src/LargestNumber.java
@@ -2,21 +2,29 @@ import java.util.Scanner;
 
 public class LargestNumber {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int a =in.nextInt();
-        int b =in.nextInt();
-        int c =in.nextInt();
+     //   Scanner in = new Scanner(System.in);
+     //   int a =in.nextInt();
+     //   int b =in.nextInt();
+     //   int c =in.nextInt();
         // Find the largest of three numbers ?
 
-        int max =a;
-        if(b> max) {
-            max = b;
-        }
-        if (c> max){
-            max=c;
-        }
-        System.out.println(max);
-
+    //    int max =a;
+     //   if(b> max) {
+     //       max = b;
+      //  }
+      //  if (c> max){
+        //    max=c;
+      //  }
+      //  int max = Math.max(c, Math.max(a , b));
+        System.out.println(Math.max(45,58));
     }
-
 }
+// int max = 0;
+// if (a>b) {
+ //    max=a;
+// } else {
+//    max =b;
+// if (c>b) {
+//    max = c ;
+//   }
+//    System.out.println(max);
